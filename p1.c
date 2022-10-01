2ce7cbe5cf14b972fd1c7e32f6deda2586a9d015
Devansh Rautela <maildevanshrautela@gmail.com>
2022-10-01 13:32:37 +0530

    01/10/22

--- p1.c
+++ p1.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h> 
 
@@ -7,4 +7,19 @@ fork();
 printf("Process id = %d\n",getpid()); 
 return 0;
 }
+*/
 
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+int main()
+{
+  
+    // make two process which run same
+    // program after this instruction
+    fork();
+  
+    printf("Hello world!\n");
+    return 0;
+}
