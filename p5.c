b026fab46abf9df856c54cf35c3955c7c9aa4575
Devansh Rautela <maildevanshrautela@gmail.com>
2022-10-01 13:32:14 +0530

    01/10/22

--- p5.c
+++ p5.c
@@ -1,3 +1,13 @@
 #include<stdio.h>
 #include<sys/types.h>
 #include<unistd.h>
+
+int main()
+{
+    int n;
+    printf("Enter the Number of Process\n");
+    scanf("%d",&n);
+    printf("Process ID: %d\n");
+
+    return 0;
+}
