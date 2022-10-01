b5c50dd624314a92d43b72f6cce12d525577401c
Devansh Rautela <maildevanshrautela@gmail.com>
2022-10-01 13:34:34 +0530

    test

--- p4.c
+++ p4.c
@@ -7,7 +7,7 @@ int pi=fork();
 if(pi==0)
 {
      sleep(5);
-     printf("Child Process_pid= %d \nParent_pid= %d\n",getpid(),getppid());}
+     printf("\nChild Process_pid= %d \nParent_pid= %d\n",getpid(),getppid());}
 else{
      printf("Child Process_pid= %d \nParent_pid= %d\n",getpid(),getppid());}
 return 0;
