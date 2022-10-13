dfd6b511961b33e68f362f9db91b62af57a5e4d6
Devansh Rautela <maildevanshrautela@gmail.com>
2022-10-14 00:04:25 +0530

    commit

--- fcfs.cpp
+++ fcfs.cpp
@@ -26,80 +26,114 @@ Step 4-> In int main()
 Stop*/
 #include <stdio.h>
 #include <stdlib.h>
+
 struct Process
 {
-    int pid;
-    int atime;
-    int btime;
-    int ctime;
-    int tatime;
-    int wtime;
-    int rtime;
-    int stime;
+    int pid; // Process ID
+    int at;  // Arrival Time
+    int bt;  // Burst Time
+    int ct;  // Completion Time
+    int tat; // Turn Around Time
+    int wt;  // Waiting Time
+    int rt;  // Respose Time
+    int st;  // Start Time
 };
+
 int compare(const void *p1, const void *p2)
 {
-    int a = ((struct Process *)p1)->atime;
-    int b = ((struct Process *)p2)->atime;
+    int a = ((struct Process *)p1)->at;
+    int b = ((struct Process *)p2)->at;
+
     if (a < b)
         return -1;
     else
         return 1;
 }
+
 int main()
 {
-    int n;
-    int swtime = 0, statime = 0, awtime = 0, atatime = 0, cyclelength = 0;
-    float cpu_ut, thro;
-    printf("Enter the number of processes: ");
+    int n;                   // Number of Process
+    float swt = 0, stat = 0; // Sum of Waiting Time and Sum of Turn Around Time
+    float cu = 0;            // CPU Utilization
+    float awt = 0, atat = 0; // Average Waiting Time and Average Turn Around Time
+    int sbt = 0;             // Sum of Burst Time
+    float srt = 0, art = 0;  // Sum of Response Time and Average Response Time
+    float thput = 0;         // Throughput
+
+    printf("Ayush Maheshwari\tSection - C\tRoll No. - 15\n\n");
+
+    printf("Enter the number of processes : ");
     scanf("%d", &n);
+
     struct Process p[n];
+
     for (int i = 0; i < n; i++)
     {
-        printf("For Process %d ", i + 1);
+        printf("For Process %d: ", i + 1);
         p[i].pid = i + 1;
-        printf("Enter the value of AT & BT: ");
-        scanf("%d %d", &p[i].atime, &p[i].btime);
+
+        printf("Enter the value of AT and BT: ");
+        scanf("%d %d", &p[i].at, &p[i].bt);
     }
+
     qsort((void *)p, n, sizeof(struct Process), compare);
-    int idle = 0;
+
     for (int i = 0; i < n; i++)
     {
+        // when first time CPU assign to the process
         if (i == 0)
         {
-            p[i].ctime = p[i].atime + p[i].btime;
-            p[i].stime = p[i].atime;
+            p[i].st = p[i].at;
+            p[i].ct = p[i].at + p[i].bt;
         }
-        else if (p[i - 1].ctime <= p[i].atime)
+        // when CPU assign after context switch
+        else if (p[i - 1].ct <= p[i].at)
         {
-            p[i].ctime = p[i].atime + p[i].btime;
-            idle += p[i].atime - p[i - 1].ctime;
-            p[i].stime = p[i].atime;
+            p[i].st = p[i].at;
+            p[i].ct = p[i].at + p[i].bt;
         }
+        // when CPU assign after any process
         else
         {
-            p[i].ctime = p[i - 1].ctime + p[i].btime;
-            p[i].stime = p[i - 1].ctime;
+            p[i].st = p[i - 1].ct;
+            p[i].ct = p[i - 1].ct + p[i].bt;
         }
-        p[i].tatime = p[i].ctime - p[i].atime;
-        p[i].wtime = p[i].tatime - p[i].btime;
-        swtime += p[i].wtime;
-        statime += p[i].tatime;
-        p[i].rtime = p[i].stime - p[i].atime;
-    }
-    awtime = swtime / n;
-    atatime = statime / n;
-    cyclelength = p[n - 1].ctime - p[0].stime;
-    cpu_ut = (cyclelength - idle) * 100;
-    thro = (float)n / cyclelength;
 
-    printf("\nPID\tAT\tBT\tCT\tTAT\tWT\tRT\n");
+        p[i].tat = p[i].ct - p[i].at;
+        p[i].wt = p[i].tat - p[i].bt;
+
+        sbt += p[i].bt;
+        swt += p[i].wt;
+        stat += p[i].tat;
+        p[i].rt = p[i].wt;
+        srt += p[i].rt;
+    }
+
+    awt = swt / n;
+    atat = stat / n;
+    art = srt / n;
+    float max;
+
     for (int i = 0; i < n; i++)
     {
-        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].pid, p[i].atime, p[i].btime, p[i].ctime, p[i].tatime, p[i].wtime, p[i].rtime);
+        max = 0;
+        if (p[i].ct > max)
+        {
+            max = p[i].ct;
+        }
     }
-printf("\nSum of Turn Around Time: %d\nAverage of Turn Around Time: %d\n",statime, atatime);
-printf("Sum of Waiting Time: %d\nAverage of Waiting Time:%d\n",swtime,awtime);
-printf("CPU Utilization: %6.2f\nThroughput: %f\n",cpu_ut,thro);
-return 0;
+
+    thput = n / max;
+    cu = (sbt / max) * 100;
+
+    printf("\nPID\tAT\tBT\tST\tCT\tTAT\tWT\tRT\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("P%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n", p[i].pid, p[i].at, p[i].bt, p[i].st, p[i].ct, p[i].tat, p[i].wt, p[i].rt);
+    }
+
+    printf("\nAverage Response Time : %f\nAverage of Turn Around Time : %f\n", art, atat);
+    printf("Average of Waiting Time : %f\nThroughput : %f\nCPU utilization : %f", awt, thput, cu);
+
+    return 0;
 }
